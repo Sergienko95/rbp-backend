7c8b6767d6f2dc1af4c953c975a46cefb48c6bc8
Jana Sergienko <jana_monkey@mail.ru>
2023-08-17 11:44:57 +0300

    one click test

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ import fastapi
 
 app = fastapi.FastAPI()
 
+
 @app.get("/")
 async def handle_hello_world():
-    return "hello"
+    return "hello world"
+
+
+@app.get("/languages")
+async def handle_languages():
+    return {
+        "jana": "python",
+    }
+
 
 @app.get("/favicon.ico")
 async def handle_favicon():
     with open("favicon.jpg", "rb") as file_object:
         image = file_object.read()
 
-    return fastapi.Response(image, media_type="image/jpeg")
+    return fastapi.Response(
+        image,
+        media_type="image/jpeg",
+    )
