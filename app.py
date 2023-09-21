d2173bd6dec7f2970454ce8bec3923088e9b9e2b
Jana Sergienko <jana_monkey@mail.ru>
2023-09-21 20:37:59 +0300

    reviews: read all, create

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import fastapi
+from devtools import debug
 
 app = fastapi.FastAPI()
+
+""" 
+    1.   Сделать запрос на сервак и получить все отзовы.
+    1.5. Сделать запрос на сервер и создать отзыв.
+    2.   Сделать запрос на сервак и получить один отзыв.
+    3.   Сделать запрос на сервак и обновить отзыв один.
+    4.   Сделать запрос на сервак и удалить отзыв один.
+"""
+
+# Сделать get запрос на /reviews
+@app.get("/reviews")
+async def handle_get_all_reviews():
+    reviews = []
+    return reviews
+
+
+# Сделать post запрос на /reviews
+@app.post("/reviews", status_code=201)
+async def handle_create_review(review: dict=fastapi.Body(), user_agent: str=fastapi.Header('user-agent')):
+    debug (review, user_agent)
+    return review
 
 
 @app.get("/")
--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import requests
-
+from devtools import debug
+from random import randint
 
 def test_root():
     response = requests.get("http://localhost:8000/")
@@ -11,3 +12,17 @@ def test_languages():
     response = requests.get("http://localhost:8000/languages")
     assert response.status_code == 200
     assert response.json() == {"jana": "python"}
+
+
+def test_reviews():
+    # Проверяю, что сначала отзывов нет
+    response = requests.get("http://localhost:8000/reviews")
+    assert response.status_code == 200
+    assert response.json() == []
+
+    # Создаем отзыв
+    review = {'subject': 'Python', 'text': 'I love Python', 'mark': randint(0, 10)}
+    response = requests.post("http://localhost:8000/reviews", json=review)
+    debug(response.headers)
+    assert response.status_code == 201
+    assert response.json() == review
