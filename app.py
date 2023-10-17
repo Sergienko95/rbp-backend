ecad76841d697906bbdea31f9a2f7e0062b0d62e
Jana_Sergienko <jana_monkey@mail.ru>
2023-10-17 10:57:35 +0300

    Reviews (#2)

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import fastapi
 from devtools import debug
 
+from db import Review, get_saved_reviews, delete_all_reviews, save_review
+
 app = fastapi.FastAPI()
 
 """ 
@@ -11,38 +13,22 @@ app = fastapi.FastAPI()
     4.   Сделать запрос на сервак и удалить отзыв один.
 """
 
+
 # Сделать get запрос на /reviews
 @app.get("/reviews")
 async def handle_get_all_reviews():
-    reviews = []
+    reviews = await get_saved_reviews()
     return reviews
+
+
+@app.delete("/reviews", status_code=204)
+async def handle_delete_all_reviews():
+    await delete_all_reviews()
 
 
 # Сделать post запрос на /reviews
 @app.post("/reviews", status_code=201)
-async def handle_create_review(review: dict=fastapi.Body(), user_agent: str=fastapi.Header('user-agent')):
-    debug (review, user_agent)
+async def handle_create_review(review: Review = fastapi.Body()):
+    debug(review)
+    await save_review(review)
     return review
-
-
-@app.get("/")
-async def handle_hello_world():
-    return "hello world"
-
-
-@app.get("/languages")
-async def handle_languages():
-    return {
-        "jana": "python",
-    }
-
-
-@app.get("/favicon.ico")
-async def handle_favicon():
-    with open("favicon.jpg", "rb") as file_object:
-        image = file_object.read()
-
-    return fastapi.Response(
-        image,
-        media_type="image/jpeg",
-    )
--- test_app.py
+++ test_app.py
@@ -1,28 +1,47 @@
 import requests
 from devtools import debug
 from random import randint
-
-def test_root():
-    response = requests.get("http://localhost:8000/")
-    assert response.status_code == 200
-    assert response.json() == "hello world"
-
-
-def test_languages():
-    response = requests.get("http://localhost:8000/languages")
-    assert response.status_code == 200
-    assert response.json() == {"jana": "python"}
+import pytest
 
 
 def test_reviews():
+    
     # Проверяю, что сначала отзывов нет
     response = requests.get("http://localhost:8000/reviews")
     assert response.status_code == 200
     assert response.json() == []
 
     # Создаем отзыв
-    review = {'subject': 'Python', 'text': 'I love Python', 'mark': randint(0, 10)}
+    review = {
+        "subject": "Python",
+        "text": "I love Python",
+        "mark": randint(0, 10),
+    }
     response = requests.post("http://localhost:8000/reviews", json=review)
-    debug(response.headers)
     assert response.status_code == 201
     assert response.json() == review
+
+     # Проверяю, созданный отзыв сохранился на сервере
+    response = requests.get("http://localhost:8000/reviews")
+    assert response.status_code == 200
+    assert response.json() == [review]
+
+    # Удаляю все отзывы 
+    response = requests.delete("http://localhost:8000/reviews")
+    assert response.status_code == 204
+    response = requests.get("http://localhost:8000/reviews")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_invalid_create():
+    # Создаем отзыв
+    review = {
+        "subject": "Python",
+        "text": "I love Python",
+        "mark": randint(0, 10),
+        "x": 1,
+    }
+    response = requests.post("http://localhost:8000/reviews", json=review)
+    debug(response.text)
+    assert response.status_code == 422
